5d98bf3e6a687960cccf529f63047b3678994acc
mingyukang5420 <mingyukang@kakao.com>
2026-07-31 17:36:04 +0900

    refactor: extend Member with loginId/password for real signup

--- skala-shop/src/main/java/com/skala/shop/domain/member/MemberRepository.java
+++ skala-shop/src/main/java/com/skala/shop/domain/member/MemberRepository.java
@@ -1,6 +1,12 @@
 package com.skala.shop.domain.member;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findByLoginId(String loginId);
+
+	boolean existsByLoginId(String loginId);
 }

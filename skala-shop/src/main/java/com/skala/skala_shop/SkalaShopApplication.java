780d0e0fabaa6c796a0ac0bac5ca11947f0ff8ce
mingyukang5420 <mingyukang@kakao.com>
2026-07-31 14:22:52 +0900

    chore: bootstrap spring boot skeleton and configure step 0 settings

---
+++ skala-shop/src/main/java/com/skala/skala_shop/SkalaShopApplication.java
@@ -0,0 +1,13 @@
+package com.skala.skala_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkalaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkalaShopApplication.class, args);
+	}
+
+}
---
+++ skala-shop/src/test/java/com/skala/skala_shop/SkalaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skala.skala_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkalaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
